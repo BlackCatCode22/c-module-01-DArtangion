7f31eeee4d001058ddc466c951b108215ffa58ea
DArtangion <dartangionzmartin@gmail.com>
2025-01-26 21:49:03 -0800

    Add files via upload

---
+++ simpleGreeting.cpp
@@ -0,0 +1,14 @@
+// Created by darta on 1/26/2025.
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string userName;
+
+    std::cout << "Enter your name: ";
+    std::cin >> userName;
+
+    std::cout << "Hello, " << userName << "! Welcome! It's nice to meet you!" << std::endl;
+
+    return 0;
+}
