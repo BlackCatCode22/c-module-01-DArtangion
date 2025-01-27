7be4d0894538f60a08a53b325060f10ae73058db
DArtangion <dartangionzmartin@gmail.com>
2025-01-26 16:48:43 -0800

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,22 @@
+// Hello World
+#include <iostream>
+using namespace std;
+
+int sumTwoints(int num1, int num2) {
+    return num1 + num2;
+}
+
+int main() {
+    cout << "\n\n Hello, World! \n\n" << endl;
+
+    cout << sumTwoints(2, 3) << endl;
+
+    cout << "\n\n\n";
+
+
+    return 0;
+}
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
